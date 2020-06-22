780c02cd3593bc1f4f39b0b2efdf2de6b8d042e1
Bilguun <bilguun@BILGUUN.local>
2020-06-22 17:12:00 +0800

    htoefgn

--- kanban-board/src/Todo.js
+++ kanban-board/src/Todo.js
@@ -1,10 +1,21 @@
-import React from "react";
+import React, { useState, useContext } from 'react';
+import { TodoContext } from './provider/prover';
+import './main.scss';
 
-const App = () => {
 
-    return (<div className="Todo Pages">
+const AddTodo = () => {
+    let { addTodo } = useContext(TodoContext);
+    const [name, setName] = useState('');
+
+    return (<div className="Pages">
         <h1>Todo</h1>
+        <div style={{display: 'flex', justifyContent: 'center'}}>
+            <input value={name} onChange={({target}) => setName(target.value)}/>
+            <button onClick = {() => {
+                addTodo(name)
+            }}> add todo </button>
+        </div>
     </div>)
 }
 
-export default App;
+export default AddTodo;
